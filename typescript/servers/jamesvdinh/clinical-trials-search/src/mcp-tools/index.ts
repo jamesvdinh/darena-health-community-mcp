ff05da692b18a215fcc0a425708604d5e48c9ce3
James Dinh <jamesdinh77@gmail.com>
2025-09-07 20:14:38 -0700

    updated index.ts

--- typescript/servers/jamesvdinh/clinical-trials-search/src/mcp-tools/index.ts
+++ typescript/servers/jamesvdinh/clinical-trials-search/src/mcp-tools/index.ts
@@ -1,8 +1,5 @@
 import { FindPatientIdToolInstance } from "./find-patient-id-tool";
-import { ReadFhirResourceToolInstance } from "./read-fhir-resource-tool";
 import { GetFhirResourceToolInstance } from "./get-patient-fhir-resource";
-import { GetPatientAttributeToolInstance } from "./get-patient-attribute-tool";
-import { CalculateCvdRiskToolInstance } from "./calculate-cvd-risk-tool";
 import { GetClinicalTrialsInstance } from "./get-clinical-trials-tool";
 import { GetFaqInstance } from "./get-faq-tool";
 import { GetMatchingClinicalTrialsInstance } from "./get-matching-clinical-trials";
@@ -11,10 +8,7 @@ import { GetEthicalTrialsInstance } from "./get-trials-eligibility-ethics-safety
 
 export {
   FindPatientIdToolInstance,
-  ReadFhirResourceToolInstance,
   GetFhirResourceToolInstance,
-  GetPatientAttributeToolInstance,
-  CalculateCvdRiskToolInstance,
   GetClinicalTrialsInstance,
   GetFaqInstance,
   GetMatchingClinicalTrialsInstance,
